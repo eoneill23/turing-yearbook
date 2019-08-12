bfee82c089c3920ff04415fe28a25e48cfe23163
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-12 17:35:31 -0600

    Get cards to appear on DOM and begin styling

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -2,8 +2,17 @@ import React from 'react';
 import Person from './Person';
 import './Cohort.css';
 
-function Cohort() {
-  
+let Cohort = (props) => {
+
+let peopleList = props.people.map(person => (
+  <Person name={person.name} quote={person.quote} superlative={person.superlative} photo={person.photo} id={person.id}/>
+  ))
+
+  return (
+    <main className="cardsDisplay">
+      {peopleList}
+    </main>
+  );
 }
 
 export default Cohort;
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Person.css';
 
-function Person() {
-  
+let Person = ({name, quote, superlative, photo, id}) => {
+  return (
+    <section className="personCard" key={id}>
+      <img src={photo}></img>
+      <p>{name}</p>
+      <p>{quote}</p>
+      <p>{superlative}</p>
+    </section>
+  )
 }
 
 export default Person;
