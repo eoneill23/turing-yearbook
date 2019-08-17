5c96a8f4a11e2bb1dbdd26a1d8e29965e66bcc08
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-17 17:16:21 -0600

    Finalize NewStudentForm testing

--- src/components/NewStudentForm.test.js
+++ src/components/NewStudentForm.test.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import NewStudentForm from './NewStudentForm.js';
 
 describe('Form', () => {
-  
+  it('should match the snapshot with all the data passed in correctly', () => {
+    const addStudentMock = jest.fn();
+    const wrapper = shallow(<NewStudentForm addStudent={addStudentMock}/>);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should fire the handleChange method when change is detected in the input fields', () => {
+    const addStudentMock = jest.fn();
+    const wrapper = shallow(<NewStudentForm addStudent={addStudentMock}/>);
+
+    wrapper.find('input').at(0).simulate('change', {target: { value: 'hello', name: 'title' } });
+    wrapper.find('input').at(1).simulate('change', {target: { value: 'my friend', name: 'description' } });
+
+    expect(wrapper.state('title')).toEqual('hello');
+    expect(wrapper.state('description')).toEqual('my friend');
+  })
 });
