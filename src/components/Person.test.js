f942135ec3000247fde29d0f2ab5d0f66b354d57
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-17 16:56:36 -0600

    Finalize person testing

--- src/components/Person.test.js
+++ src/components/Person.test.js
@@ -17,4 +17,34 @@ describe('Person', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should match the snapshot if students is passed as the type prop', () => {
+    const wrapper = shallow(<Person 
+      name={'Travis'}
+      quote={'Cheers'}
+      superlative={'Most likely to say cheers'}
+      photo={''}
+      id={1}
+      deleteStudent={jest.fn()}
+      type={'students'}
+    />)
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call the delete student prop when the delete button is clicked', () => {
+    const deleteStudentMock = jest.fn();
+    const wrapper = shallow(<Person 
+      name={'Travis'}
+      quote={'Cheers'}
+      superlative={'Most likely to say cheers'}
+      photo={''}
+      id={1}
+      deleteStudent={deleteStudentMock}
+      type={'students'}
+    />)
+
+    wrapper.find('button').simulate('click');
+
+    expect(deleteStudentMock).toHaveBeenCalled();
+  })
 });
